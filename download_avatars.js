5d88403cb4e0d05ec600bf8cb4b86dca42916fd4
Lucas Cruz <lucas_ftc@hotmail.com>
2019-01-15 00:11:21 +0000

    add save image function

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
-// const request = require('request'),
+const request = require('request'),
+      fs      = require('fs'),
       secret  = require('./secret');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -12,7 +13,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   request(options, (err, res, body) => {
     var data = JSON.parse(body);
-
     cb(err, data);
   });
 }
@@ -21,8 +21,16 @@ function getAllUserAvatars(err, data) {
   data.forEach((user) => console.log(user.avatar_url));
 }
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-getRepoContributors("jquery", "jquery", getAllUserAvatars)
+function downloadImageByURL(url, filePath) {
+  request(url)
+    .on('error', (err) => {
+      throw err;
+    })
+    .pipe(fs.createWriteStream(filePath));
+}
 
+// console.log('Welcome to the GitHub Avatar Downloader!');
+// getRepoContributors("jquery", "jquery", getAllUserAvatars)
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
 
 
